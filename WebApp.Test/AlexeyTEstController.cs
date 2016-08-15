139bb5d5c349b05b3f23565ac2a5fe9bd3c45745
Alexey Stupakov <alexey.stupakov@gmail.com>
2016-08-15 14:46:23 +0300

    Added test librarys

--- WebApp.Test/AlexeyTEstController.cs
+++ WebApp.Test/AlexeyTEstController.cs
@@ -12,8 +12,40 @@ using WebApp.Entities;
 
 namespace WebApp.Test
 {
-    public class AlexeyTEstController
+    public class AlexeyTestController
     {
-        
+        private AlexController _controller;
+        private Mock<IRepositoryServise> _obj;
+        private List<TestModel> mockObj = new List<TestModel>()
+        {
+            new TestModel {Id = 1, Title = "Title 1", Body = "Body 1" },
+            new TestModel {Id = 2, Title = "Title 2", Body = "Body 2" },
+            new TestModel {Id = 3, Title = "Title 3", Body = "Body 3" }
+        };
+
+        //public void Test_For_Method_GetAll ()
+        //{
+        //    _obj = new Mock<IRepositoryServise>();
+        //    _obj.Setup(o => o.GetAllTestModels()).Returns();
+        //    _controller = new AlexController();
+
+        //}
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
     }
 }
